dde9a14318fe075e8d1117cee0c54a5ac83ad298
rogerdepaulaa <roger.philippe.18@gmail.com>
2022-08-19 15:12:12 -0300

    componets

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,18 @@
-import './styles.css'
+import './styles.css';
+import { Card }from '../../components/card';
 
 function Home() {
   return (
     <div className='container'>
       <h1>Lista de presença</h1>
-      <input type="text" placeholder="Digite um nome ..." />
+      <input type="text" placeholder="Digite um nome ..." required />
       <button type="button">Adicionar</button>
+
+      <Card  name="Roger" time="15:15:25"/>
+      <Card  name="Philippe" time="15:16:05"/>
+
+
     </div>
   )
 }
-export default Home;
+export default Home; 
