adb62413d9e9a12703246c9072d4df66398c8d69
rogerdepaulaa <roger.philippe.18@gmail.com>
2022-08-19 17:26:58 -0300

    api github name avatar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
+
 import { Card } from "../../components/card";
 
 export function Home() {
   const [studentName, setStudentName] = useState();
   const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({name: '', avatar: ''});
 
   function handleAddStudent() {
     const newStudent = {
@@ -18,10 +20,27 @@ export function Home() {
 
     setStudents((prevState) => [...prevState, newStudent]);
   }
+  useEffect (() => {
+    fetch('https://api.github.com/users/rogerdepaulaa')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url,
+        })
+    })
+  }, []);
 
   return (
     <div className="container">
-      <h1>Lista de presença</h1>
+      <header className="header">
+        <h1>Lista de presença</h1>
+              <div className="header-div">
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de Perfil" className="foto-perfil"/>
+        </div>
+      </header>
       <input
         type="text"
         placeholder="Digite um nome ..."
@@ -33,9 +52,15 @@ export function Home() {
         Adicionar
       </button>
 
-      {students.map((student) => (
-        <Card name={student.name} time={student.time} />
-      ))}
+      {
+        students.map(student => (
+          <Card 
+            key={student.time}
+            name={student.name} 
+            time={student.time} 
+          />
+        ))
+      }
     </div>
-  );
+  )
 }
