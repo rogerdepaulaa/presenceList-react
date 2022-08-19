1c3502d06de2dfb475704da45160d001a9c9e059
rogerdepaulaa <roger.philippe.18@gmail.com>
2022-08-19 14:32:12 -0300

    primeiros stilos

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+function Home() {
+  return (
+    <div className='container'>
+      <h1>Lista de presença</h1>
+      <input type="text" placeholder="Digite um nome ..." />
+      <button type="button">Adicionar</button>
+    </div>
+  )
+}
+export default Home;
